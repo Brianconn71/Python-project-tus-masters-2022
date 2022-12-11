4520d34d164a481a73a3b097a59dcbf3ae623c40
Brian Connolly <brian.connolly71@gmail.com>
2022-12-11 22:17:28 +0000

    tests

--- test_calculations.py
+++ test_calculations.py
@@ -1,23 +1,21 @@
 from calculations import *
 import pytest
 
-def test_read_data_and_create_lists():
-    assert 1 ==1
-
-
 def test_get_number_of_games_in_season():
-    pass
+    output = get_number_of_games_in_season()
+    assert output == 380
 
-def test_get_average_fouls_per_game():
-    assert get_average_fouls_per_game(380) == 380
-
+def test_get_total_fouls_per_game_in_season():
+    output = get_total_fouls_per_game_in_season()
+    assert len(output) == 380
+    assert type(output) == list
 
 if __name__ == "__main__":
     # calls the read_data_and_create_lists in calculations.py to read data from csv and split into lists
     read_data_and_create_lists()
+    # get_total_fouls_per_game_in_season()
     # uses the length of the home_fouls list (always has at least one value) to calculate total games
     total_games = len(home_fouls)
     # gets the average fouls per game in season from get_average_fouls_per_game function in calculations.py
     average_fouls = get_average_fouls_per_game(total_games)
-    test_get_average_fouls_per_game()
-    pytest.main(["__file__", "-vv"])
+    pytest.main(["test_calculations.py", "-vv"])
