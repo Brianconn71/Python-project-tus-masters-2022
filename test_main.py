3a5fe53442644d66d18c6fa3e237b1a754c788aa
bconnolly <brian@payslip.com>
2022-12-18 14:01:23 +0000

    new test

--- test_main.py
+++ test_main.py
@@ -3,7 +3,7 @@
 This file is used to test the main.py file
 """
 from main import (
-    print_statements
+    print_statements, correlation_and_dictionary
 )
 import pytest
 
@@ -15,6 +15,18 @@ def test_print_statements():
     """
     # calls the print_startements function
     print_statements()
+    # asserts the returned value is equal to the expected value.
+    assert True
+
+
+def test_correlation_and_dictionary():
+    """
+    tests correlation_and_dictionary prints the correct
+    data to the console and analyses correlation between
+    two lists and the dictionary data
+    """
+    # calls the print_startements function
+    correlation_and_dictionary()
     # asserts the returned value is equal to the expected value.
     assert True
 
